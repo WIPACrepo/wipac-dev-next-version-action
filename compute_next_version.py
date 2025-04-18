a71806d3d7d9a44e2961c4dc4688975c22050dc8
ric-evans <emejqz@gmail.com>
2025-04-18 13:53:14 -0500

    logging

--- compute_next_version.py
+++ compute_next_version.py
@@ -1,15 +1,30 @@
 """A script for determining the next version of a package."""
 
 import enum
+import fnmatch
 import logging
 import os
-from fnmatch import fnmatch
 
 
 class BumpType(enum.StrEnum):
     MAJOR = enum.auto()
     MINOR = enum.auto()
     PATCH = enum.auto()
+
+
+def are_all_files_ignored(changed_files: list[str], ignore_patterns: list[str]) -> bool:
+    """Do all the changed files match the ignore_patterns?"""
+    for f in changed_files:
+        for pat in ignore_patterns:
+            if fnmatch.fnmatch(f, pat):
+                logging.debug(f"File is  ignored: {f} (by pattern: {pat})")
+                break
+            else:
+                logging.debug(f"File not ignored: {f} (by pattern: {pat})")
+        else:  # <- if no 'break'
+            logging.info(f"Found a changed non-ignored file: {f}")
+            return False
+    return True
 
 
 def main(
@@ -29,7 +44,7 @@ def main(
     # is a release needed?
     if not changed_files:
         return logging.info("No changes detected")
-    if all(any(fnmatch(f, pat) for pat in ignore_patterns) for f in changed_files):
+    if are_all_files_ignored(changed_files, ignore_patterns):
         return logging.info("None of the changed files require a release.")
 
     # detect bump
@@ -37,9 +52,7 @@ def main(
         bump = BumpType.MAJOR
     elif "[minor]" in commit_log:
         bump = BumpType.MINOR
-    elif ("[patch]" in commit_log) or ("[fix]" in commit_log):
-        bump = BumpType.PATCH
-    elif force_patch:
+    elif ("[patch]" in commit_log) or ("[fix]" in commit_log) or force_patch:
         bump = BumpType.PATCH
     else:
         return logging.info("Commit log doesn't signify a version bump.")
