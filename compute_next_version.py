9826ff0ec6193885c1759be6fead519cc92e9846
ric-evans <emejqz@gmail.com>
2025-10-29 11:37:11 -0500

    update error

--- compute_next_version.py
+++ compute_next_version.py
@@ -39,10 +39,11 @@ class EnvConfig:
         for pat in self.IGNORE_PATHS:
             if pat.endswith("/"):
                 raise RuntimeError(
-                    "ignore-paths cannot end with '/' — "
-                    "to define a directory's contents use 'foo/**', or a specific file use 'foo/file' "
-                    "(this restriction is due to a discrepancy between "
-                    "'.gitignore'-style semantics and the 'pathspec' package.)"
+                    f"ignore-path cannot end with '/' ({pat}) — "
+                    "to define a directory's contents use "
+                    "'foo/**', 'foo/*', or (for a specific file) 'foo/file'. "
+                    "This restriction is due to a discrepancy between "
+                    "'.gitignore'-style semantics and the 'pathspec' package."
                 )
 
         object.__setattr__(
